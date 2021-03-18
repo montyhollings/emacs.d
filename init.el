322b3c14609a09a84e6dde62161753c9a71a3d8d
Monty Hollings <hollings@protonmail.com>
2021-03-18 16:17:29 +0000

    update

--- init.el
+++ init.el
@@ -29,8 +29,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("c:/users/Monty-PC/org/work.org" "c:/users/Monty-PC/org/college.org")))
+ '(org-agenda-files '("~/org/work.org" "~/org/main.org" "~/org/college.org"))
+ '(package-selected-packages
+   '(org-onenote zenburn-theme use-package org-superstar mixed-pitch gruvbox-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
