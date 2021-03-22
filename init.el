c0ed8a789ce767b9d1cafcda208929092955f785
Monty Hollings <hollings@protonmail.com>
2021-03-22 09:18:49 +0000

    new theme by default

--- init.el
+++ init.el
@@ -4,9 +4,7 @@
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-
-
-(load-theme 'gruvbox t)
+(load-theme 'spacemacs-dark t)
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (setq recentf-max-saved-items 25)
@@ -18,7 +16,7 @@
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
 
-(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+
 (use-package mixed-pitch
   :hook
   ;; If you want it in all text modes:
@@ -30,11 +28,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/org/work.org" "~/org/main.org" "~/org/college.org"))
+ '(org-todo-keywords '((sequence "" "TODO" "DONE")))
  '(package-selected-packages
-   '(org-onenote zenburn-theme use-package org-superstar mixed-pitch gruvbox-theme)))
+   '(spacemacs-theme org-bullets org-onenote zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq org-hide-leading-stars nil)
+(put 'upcase-region 'disabled nil)
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
